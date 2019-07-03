b4c54de7da389e4f8a6aab6419069d124dfd8177
radu103 <radu103@hotmail.com>
2019-07-03 08:10:50 +0000

    front end app binded to backend dest

---
+++ frontend/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("bootcamp.frontend.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ frontend/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"bootcamp/frontend/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
